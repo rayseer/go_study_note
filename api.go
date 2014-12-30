bfbec4f8b0fc4f65719e2d7303a06b73520f5082
futurelab <futurelab@futurelabdeMac-mini.local>
2014-12-30 17:39:45 +0800

    Fix bug

--- api.go
+++ api.go
@@ -13,7 +13,7 @@ func main() {
 
 	//用户登陆
 	//curl "localhost:3000/user/login"
-	route.Post("/user/login", postLogin())
+	route.Post("/user/login", user.PostLogin())
 	//获取用户简介
 	//curl "localhost:3000/user/profile"
 	route.Get("/user/profile", user.GetProfile())
